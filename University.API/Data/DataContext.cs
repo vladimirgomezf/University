66925fb55decc8d56ab7d869fc6cfd31896640d8
Vladimir Gómez Fumero <vladimirg.fumero@gmail.com>
2022-03-22 13:06:02 -0400

    Create the DbContext File

---
+++ University.API/Data/DataContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using University.API.Models;
+
+namespace University.API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base (options) { }
+
+        public DbSet<Address> Address { get; set; }
+        public DbSet<Professor> Professor { get; set; }
+        public DbSet<Student> Student { get; set; }
+    }
+}
