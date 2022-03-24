c60343260833e3408c2cc2c0565da8c9cb8f3458
Vladimir Gómez Fumero <vladimirg.fumero@gmail.com>
2022-03-24 02:23:40 -0400

    DI for the new services and the HttpClient object

--- University.Frontend/Program.cs
+++ University.Frontend/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
+using System.Net.Http.Headers;
 using University.Frontend.Data;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -7,7 +8,13 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
-builder.Services.AddSingleton<WeatherForecastService>();
+builder.Services.AddSingleton<AddressService>();
+builder.Services.AddSingleton<StudentService>();
+builder.Services.AddSingleton<ProfessorService>();
+var client = new HttpClient();
+client.BaseAddress = new Uri("https://localhost:7110");
+client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+builder.Services.AddSingleton<HttpClient>(client);
 
 var app = builder.Build();
 
