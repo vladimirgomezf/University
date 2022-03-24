50a24d878f94aeedb3b1bd83b546b8eab221f1ad
Vladimir Gómez Fumero <vladimirg.fumero@gmail.com>
2022-03-24 11:40:08 -0400

    Create Post and Delete services

--- University.Frontend/Data/AddressService.cs
+++ University.Frontend/Data/AddressService.cs
@@ -2,6 +2,7 @@
 using System.Net.Http.Headers;
 using System.Net.Http.Json;
 using University.API.Models;
+using University.API.Controllers;
 
 namespace University.Frontend.Data
 {
@@ -18,6 +19,34 @@ namespace University.Frontend.Data
         {
             HttpResponseMessage res = await _client.GetAsync("api/address");
 
+            if (res.IsSuccessStatusCode)
+            {
+                return await res.Content.ReadFromJsonAsync<List<Address>>();
+            }
+            else
+            {
+                return new List<Address>();
+            }
+        }
+
+        public async Task<List<Address>> PostData(Address obj)
+        {
+            HttpResponseMessage res = await _client.PostAsJsonAsync("api/address", obj);
+            
+            if (res.IsSuccessStatusCode)
+            {
+                return await res.Content.ReadFromJsonAsync<List<Address>>();
+            }
+            else
+            {
+                return new List<Address>();
+            }
+        }
+
+        public async Task<List<Address>> DeleteData(int obj)
+        {
+            HttpResponseMessage res = await _client.DeleteAsync("api/address/"+ obj +"");
+            
             if (res.IsSuccessStatusCode)
             {
                 return await res.Content.ReadFromJsonAsync<List<Address>>();
