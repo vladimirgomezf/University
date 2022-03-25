86514a35d26f1c4a95582100778768ebc3390caf
Vladimir Gómez Fumero <vladimirg.fumero@gmail.com>
2022-03-25 05:34:56 -0400

    Add the busines logic to edit and delete

---
+++ University.Frontend/Pages/AddressDetailsBase.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Components;
+using University.API.Models;
+using University.Frontend.Data;
+
+namespace University.Frontend.Pages
+{
+    public class AddressDetailsBase: ComponentBase
+    {
+        public static Address address { get; set; } = new Address();
+        public List<Address> addresses { get; set; } = new List<Address>();
+
+        [Inject]
+        public AddressService addressService { get; set; }
+
+        [Parameter]
+        public int Id { get; set; }
+
+        protected async override Task OnInitializedAsync()
+        {
+            //address = await addressService.GetDataById(Id);
+            addresses = await addressService.DeleteData(Id);
+        }
+    }
+}
